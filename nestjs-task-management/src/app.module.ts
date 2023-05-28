317dd220ff5b3c8af434fd83116697b927485509
Sergei-Nikiforov <pclp@inbox.ru>
2023-05-28 23:45:25 +0300

    !

--- nestjs-task-management/src/app.module.ts
+++ nestjs-task-management/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/task.entity';
+import { ScheduleModule } from '@nestjs/schedule';
+// import { TaskScheduleModule } from './tasks/task-schedule/task-schedule.module';
 
 @Module({
   imports: [
@@ -16,7 +18,8 @@ import { Task } from './tasks/task.entity';
       entities: [Task],
       autoLoadEntities: true,
       synchronize: true,
-    })
+    }),
+//    ScheduleModule.forRoot(),
   ],
 })
 export class AppModule {}
