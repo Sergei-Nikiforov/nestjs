c29cb9fa247328101fa1912b490c1933ae7366ed
Sergei-Nikiforov <pclp@inbox.ru>
2023-06-30 11:12:09 +0300

    Configiration management

--- nestjs-task-management/src/auth/jwt.strategy.ts
+++ nestjs-task-management/src/auth/jwt.strategy.ts
@@ -4,11 +4,12 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { UsersRepository } from "./users.repository";
 import { JwtPayload } from "./jwt-payload.interface";
 import { User } from "./user.entity";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private usersRepository: UsersRepository, )  {
-        super({ secretOrKey: 'topSecret51', jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), })
+    constructor(private usersRepository: UsersRepository, private configService: ConfigService )  {
+        super({ secretOrKey: configService.get('JWT_SECRET'), jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), })
     }
 
     async validate(payload: JwtPayload): Promise<User> {
