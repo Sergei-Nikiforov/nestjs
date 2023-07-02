30c97de3f6724d7b146aa6256440d22ea43da956
Sergei-Nikiforov <pclp@inbox.ru>
2023-07-02 22:18:40 +0300

    !

--- nestjs-task-management/src/tasks/tasks.module.ts
+++ nestjs-task-management/src/tasks/tasks.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { Task } from './task.entity';
 import { TaskScheduleModule } from './task-schedule/task-schedule.module';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task]), TaskScheduleModule, AuthModule],
